dbe520d0faf02a04b3af69884d90ba2468b26b0e
kunle <kunlefes9184@gmail.com>
2023-03-12 16:43:20 +0100

    adjusted host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 AUTH_USER_MODEL = 'shop.MyUser'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['my-ecommerce-production.up.railway.app' ,'127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
