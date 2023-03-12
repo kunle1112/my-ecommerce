6e347dfe15ca6ff059406552fb907fd1c4cc284f
kunle <kunlefes9184@gmail.com>
2023-03-12 18:43:28 +0100

    adjusted register

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -111,7 +111,7 @@ DATABASES = {
     }
 }
 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
